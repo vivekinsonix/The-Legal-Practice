47e51cbd4f0d92e3bd477b833cc55ae8542c648b
unknown <viveks@insonix.com>
2026-02-05 16:58:29 +0530

    :chnages

--- .next/dev/static/chunks/app_2fdf6783._.js
+++ .next/dev/static/chunks/app_2fdf6783._.js
@@ -1410,7 +1410,7 @@ const Header = ()=>{
                         className: "flex w-full items-center justify-between gap-2",
                         children: [
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                className: "w-xl md:w-auto",
+                                className: "w-3xl md:w-auto",
                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(Logo, {}, void 0, false, {
                                     fileName: "[project]/app/components/drawer/header.tsx",
                                     lineNumber: 58,
